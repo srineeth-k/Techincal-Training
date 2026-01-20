f2666a4784b6849b7c3a86dc6ebba5a6c05d81ed
srineeth-k <srineeth21@gmail.com>
2026-01-20 12:03:03 +0530

    ATM withdrawal using Threads

---
+++ 20-01-26/ATMSimulation.java
@@ -0,0 +1,27 @@
+import java.util.TreeMap;
+
+class BankAccount{
+    private int bal=100000;
+
+    synchronized void withdraw(String name , int amt){
+
+    }
+}
+class ATMUser extends Thread{
+    BankAccount acc;
+    String name;
+    int amt;
+    ATMUser(BankAccount acc,String name,int amt){
+
+    }
+    public void run(){
+        
+    }
+    
+}
+
+public class ATMSimulation {
+    public static void main(String args[]){
+
+    }
+}
